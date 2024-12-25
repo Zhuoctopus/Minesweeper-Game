fcf3c14839204ce846b7f6835486a98f2acd80d5
zhu <zhuoctopus@gmail.com>
2024-12-25 20:29:13 +0700

    Version 1.0

--- src/com/zhu/Board.java
+++ src/com/zhu/Board.java
@@ -1,4 +1,4 @@
-package com.zetcode;
+package com.zhu;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
--- src/com/zhu/Minesweeper.java
+++ src/com/zhu/Minesweeper.java
@@ -1,16 +1,9 @@
-package com.zetcode;
+package com.zhu;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-/**
- * Java Minesweeper Game
- *
- * Author: Jan Bodnar
- * Website: http://zetcode.com
- */
 
 public class Minesweeper extends JFrame {
 
